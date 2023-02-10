a17f9f221b52c3d03541f10c4e0bdbd41ac0093a
Sait ÇAKIR <sait25@windowslive.com>
2023-02-10 11:19:19 +0300

    Add project files.

---
+++ Skill_CodeFirstEntity/Models/siniflar/CONTEXT.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Skill_CodeFirstEntity.Models.siniflar
+{
+    public class CONTEXT:DbContext
+    {
+        public DbSet<YETENEKLER> YETENEKLERS { get; set; }
+    }
+}
